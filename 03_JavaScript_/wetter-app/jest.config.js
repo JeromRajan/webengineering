cb2c0b3c0d56ce745e429eae5c8e19837093bf8c
jerom <jerom.rajan@hotmail.com>
2024-05-29 16:11:57 +0200

    Add Project

--- 03_JavaScript_/wetter-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: "jsdom",
-    transform: {
-        "^.+\\.jsx?$": "babel-jest"
-    },
-    moduleFileExtensions: ["js", "jsx"],
-    coverageProvider: "v8",
-    clearMocks: true,
-};
